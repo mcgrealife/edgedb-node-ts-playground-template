f738c9a5151f64cf2fca5fe4ba7a53fa1bedc5a0
Michael McGreal <mcgrealife@gmail.com>
2023-03-01 17:48:41 +0000

    add migration command, example schema, and gitpod start commands to move the example schema into the /dbschema directory created by edgedb project init. update src/index to use the new schema example types in the query builder!

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { createClient } from 'edgedb'
+import e from '../dbschema/edgeql-js/index.mjs'
 
 const client = createClient()
 
-const sayHello = (subject: string): void => {
-  console.log('Hello23npm inarlasdL  ' + subject)
-}
+const query = e.insert(e.Example, {
+  name: 'test name',
+})
 
-sayHello('TypeScript')
+const result = await query.run(client)
+
+console.log('edgeql query result:', result)
