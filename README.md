bf6cd4ebfe44cd8f4b866788544c0c58b92f7ab0
mcgrealife <mcgrealife@gmail.com>
2023-03-01 10:19:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-ts_nodemon_tsc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nodemon-prsbth)
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const sayHello = (subject: string): void => {
+  console.log('Hello23npm inarlL  ' + subject);
+};
+
+sayHello('TypeScript');
