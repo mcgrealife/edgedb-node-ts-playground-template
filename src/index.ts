8784c57f0d4dc1b5d70bdd7d96bd61b299a4be25
Michael McGreal <mcgrealife@gmail.com>
2023-03-02 16:12:34 +0000

    checking in composable query idea before changing

--- src/index.ts
+++ src/index.ts
@@ -5,41 +5,25 @@ const client = createClient()
 
 const productsData = [
   {
-    name: 'product1',
-    code: '123',
-    familyCode: '1234',
-    barcode: '12345',
-    color: {
-      name: 'color1',
-      code: 'color123',
-    },
-    size: {
-      name: 'size1',
-      code: 'size123',
-    },
-  },
-  {
-    name: 'product2',
-    code: '1234',
-    familyCode: '12345',
-    barcode: '123456',
-    color: {
-      name: 'color2',
-      code: 'color1234',
-    },
-    size: {
-      // name: 'size2', // to null case
-      code: 'size1234',
-    },
+    name: 'zapatilla Nike Air Power Mini 2',
+    barcode: '922922342422551529',
+    familyCode: '4973',
+    code: '4973-2792',
+    color: { name: 'azul', code: 'az' },
+    size: { name: 'test1', size: 'size1' },
   },
 ]
 
 export const insertProduct = async (products: typeof productsData) => {
   const query = e.params({ items: e.json }, (params) => {
     return e.for(e.json_array_unpack(params.items), (item) => {
-      return e.insert(e.Product, {
+      // e.op() if statement cannot be used inside e.insert()
+      // so instead, we use composable queries inside the for loop
+      // and express the if logic in javascript before the return statements!
+
+      // baseProduct expression used in either if branch
+      const baseProduct = e.insert(e.Product, {
         name: e.cast(e.str, item.name),
-        code: e.cast(e.str, item.code),
         familyCode: e.cast(e.str, item.familyCode),
         barcode: e.cast(e.str, item.barcode),
         color: e.assert_single(
@@ -50,21 +34,41 @@ export const insertProduct = async (products: typeof productsData) => {
             })
             .unlessConflict((color) => ({
               on: color.name,
-              else: color, // this returns the conflicting object!
-            }))
-        ),
-        size: e.assert_single(
-          e
-            .insert(e.Size, {
-              name: e.cast(e.str, e.json_get(item.size, 'name')), // handle null get here instead of in separare function (json_get returns an empty set if item.size.name does not exists)
-              code: e.cast(e.str, item.size.code),
-            })
-            .unlessConflict((size) => ({
-              on: size.name,
-              else: size, // this returns the conflicting object!
+              else: color,
             }))
         ),
       })
+      // here is the if logic // e.json_get() is still required, because item.size?.name always passes the size?, since size has other edgeql properties on it
+      if (e.op('exists', e.json_get(item.size, 'name'))) {
+        // this might need a e.op(e.json_get(),"=", empty set?)
+        const createdSize = e.insert(e.Size, {
+          name: e.cast(e.str, item.name.size),
+          code: e.cast(e.str, item.name.code),
+        })
+        return e.insert(e.Product, {
+          name: e.cast(e.str, item.name),
+          familyCode: e.cast(e.str, item.familyCode),
+          barcode: e.cast(e.str, item.barcode),
+          color: e.assert_single(
+            e
+              .insert(e.Color, {
+                name: e.cast(e.str, item.color.name),
+                code: e.cast(e.str, item.color.code),
+              })
+              .unlessConflict((color) => ({
+                on: color.name,
+                else: color,
+              }))
+          ),
+          size: createdSize,
+        })
+        // return e.update(e.Product, (product) => ({
+        //   // filter_single: e.op(product, '=', baseProduct),
+        //   set: {
+        //     size: createdSize,
+        //   },
+        // }))
+      } else return baseProduct
     })
   })
   const result = await query.run(client, {
@@ -73,23 +77,3 @@ export const insertProduct = async (products: typeof productsData) => {
   console.log('result----> ', result)
 }
 insertProduct(productsData)
-
-// export function insert_size_unless_conflict_select(name: string, code: string) {
-//   if (!name) {
-//     return new Set()
-//   }
-//   return e.assert_single(
-//     e
-//       .insert(e.Size, {
-//         name: e.cast(e.str, name),
-//         code: e.cast(e.str, code),
-//       })
-//       .unlessConflict((size) => ({
-//         on: size.name,
-//         else: e.select(e.Size, (size) => ({
-//           id: true,
-//           filter: e.op(size.name, '=', e.cast(e.str, name)),
-//         })),
-//       }))
-//   )
-// }
