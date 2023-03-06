981c2c1f4b8eb3b74c0dd2f81c348e5d060edb9d
Michael McGreal <mcgrealife@gmail.com>
2023-03-06 20:54:18 +0000

    final solution!

--- src/index.ts
+++ src/index.ts
@@ -12,61 +12,34 @@ const productsData = [
     barcode: randomString,
     familyCode: '4973',
     code: '4973-2792',
-    color: { name: 'azul' + randomString, code: 'az' },
+    // color: { name: 'azul' + randomString, code: 'az' },
+    // color: {},
     // size: {},
-    size: { name: 'size.name' + randomString, code: 'code1' }, // uncomment to test else branch
+    // size: { name: 'size.name' + randomString, code: 'code1' }, // uncomment to test else branch
   },
 ]
 
 export const insertProduct = async (products: typeof productsData) => {
   const query = e.params({ items: e.json }, (params) => {
     return e.for(e.json_array_unpack(params.items), (item) => {
-      if (
-        e.op('exists', e.json_get(item.size, 'name')) &&
-        e.op('exists', e.json_get(item.size, 'code'))
-      ) {
-        return e.insert(e.Product, {
-          name: e.cast(e.str, item.name),
-          code: e.cast(e.str, item.code),
-          familyCode: e.cast(e.str, item.familyCode),
-          barcode: e.cast(e.str, item.barcode),
-          color: e.assert_single(
-            e
-              .insert(e.Color, {
-                name: e.cast(e.str, item.color.name),
-                code: e.cast(e.str, item.color.code),
-              })
-              .unlessConflict((color) => ({
-                on: color.name,
-                else: color,
-              }))
-          ),
-          // if branch ignores size key
-        })
-      } else {
-        return e.insert(e.Product, {
-          name: e.cast(e.str, item.name),
-          code: e.cast(e.str, item.code),
-          familyCode: e.cast(e.str, item.familyCode),
-          barcode: e.cast(e.str, item.barcode),
-          color: e.assert_single(
-            e
-              .insert(e.Color, {
-                name: e.cast(e.str, item.color.name),
-                code: e.cast(e.str, item.color.code),
-              })
-              .unlessConflict((color) => ({
-                on: color.name,
-                else: color,
-              }))
-          ),
-          // else branch uses size key!
-          size: e.insert(e.Size, {
-            name: e.cast(e.str, item.size.name),
-            code: e.cast(e.str, item.size.code),
-          }),
-        })
-      }
+      return e.insert(e.Product, {
+        name: e.cast(e.str, item.name),
+        code: e.cast(e.str, item.code),
+        familyCode: e.cast(e.str, item.familyCode),
+        barcode: e.cast(e.str, item.barcode),
+        color: e.op('exists', e.json_get(item.color, 'name')) // 'exists' returns true if empty set (and json_get returns empty set if property does not exist)
+          ? null
+          : e.insert(e.Color, {
+              name: e.cast(e.str, item.color.name),
+              code: e.cast(e.str, item.color.code),
+            }),
+        size: e.op('exists', e.json_get(item, 'size', 'name')) // json_get can accept multiple paths arguments, i.e. if item.size might not exist also. (Though it seems to work either way)
+          ? null
+          : e.insert(e.Size, {
+              name: e.cast(e.str, item.size.name),
+              code: e.cast(e.str, item.size.code),
+            }),
+      })
     })
   })
   const result = await query.run(client, {
