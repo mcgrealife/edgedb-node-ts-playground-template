cece556b5665598dcc049d25a41443b7a40a4a55
Michael McGreal <mcgrealife@gmail.com>
2023-03-07 20:21:36 +0000

    update example schema and query buidler code to show linking. typing e.P and e.C also has less namespace clutter than e.E does

--- default.esdl
+++ default.esdl
@@ -1,9 +1,13 @@
-# What is this?: This type Example only exists for the example query builder in src/index.ts
-# You can change type Example to anything. See Edgedb schema modeling documentation at https://www.edgedb.com/docs/datamodel/index
+# Edgedb schema docs https://www.edgedb.com/docs/datamodel/index
 
 module default {
 
-  type Example {
+  type Parent {
+    property name -> str;
+    multi link children -> Child;
+  }
+
+  type Child {
     property name -> str;
   }
 
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 // To execute this edgedb query, `npm run start`
 // For 'e' to access your latest schema, `npm run migrate`
+// To view your database, `npm run ui`
 
 import { createClient } from 'edgedb'
 import e from '../dbschema/edgeql-js/index.mjs'
 
 const client = createClient()
 
-const query = e.insert(e.Example, {
-  name: 'test name',
+const query = e.insert(e.Parent, {
+  name: 'parent',
+  children: e.insert(e.Child, {
+    name: 'child',
+  }),
 })
 
 const result = await query.run(client)
 
-console.log('edgeql query result:', result)
+console.log('query result:', result)
