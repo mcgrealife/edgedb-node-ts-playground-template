f176639e793e02e3e6d9c65194c41484381ae4cd
Michael McGreal <mcgrealife@gmail.com>
2023-03-08 17:32:55 +0000

    WOW THIS FOR LOOP WORKS as a condition insert workaround! thanks to https://discord.com/channels/841451783728529451/1071024789243822130/1072145084843298967

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,26 @@ import e from '../dbschema/edgeql-js/index.mjs'
 const client = createClient()
 
 // random strings to re-run script without encountering exclusive contraint error
-const randomString = Math.ceil(Math.random() * 10000).toString()
+const randomString = () => Math.ceil(Math.random() * 10000).toString()
 
 const productsData = [
   {
     name: 'zapatilla Nike Air Power Mini 2',
-    barcode: randomString,
+    barcode: randomString(),
     familyCode: '4973',
     code: '4973-2792',
-    // color: { name: 'azul' + randomString, code: 'az' },
+    color: { name: 'azul' + randomString(), code: 'az' },
+    // size: { name: 'size.name' + randomString(), code: 'code1' }, // uncomment to test else branch
+  },
+  {
+    name: 'zapatilla Nike Air Power Mini 3',
+    barcode: randomString(),
+    familyCode: '4973',
+    code: '4973-2792',
+    // color: { name: 'azul' + randomString(), code: 'az' },
     // color: {},
     // size: {},
-    // size: { name: 'size.name' + randomString, code: 'code1' }, // uncomment to test else branch
+    // size: { name: 'size.name' + randomString(), code: 'code1' }, // uncomment to test else branch
   },
 ]
 
@@ -27,18 +35,15 @@ export const insertProduct = async (products: typeof productsData) => {
         code: e.cast(e.str, item.code),
         familyCode: e.cast(e.str, item.familyCode),
         barcode: e.cast(e.str, item.barcode),
-        color: e.op('exists', e.json_get(item.color, 'name')) // 'exists' returns true if empty set (and json_get returns empty set if property does not exist)
-          ? null
-          : e.insert(e.Color, {
-              name: e.cast(e.str, item.color.name),
-              code: e.cast(e.str, item.color.code),
-            }),
-        size: e.op('exists', e.json_get(item, 'size', 'name')) // json_get can accept multiple paths arguments, i.e. if item.size might not exist also. (Though it seems to work either way)
-          ? null
-          : e.insert(e.Size, {
-              name: e.cast(e.str, item.size.name),
-              code: e.cast(e.str, item.size.code),
-            }),
+        color: e.for(
+          e.set(e.json_get(item, 'color', 'name')),
+          (optionalItem) => {
+            return e.insert(e.Color, {
+              name: e.cast(e.str, optionalItem),
+              code: 'test',
+            })
+          }
+        ),
       })
     })
   })
