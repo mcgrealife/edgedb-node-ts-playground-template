b761bb1bae604bb51209067ade16c468dad42257
Michael McGreal <mcgrealife@gmail.com>
2023-03-08 17:45:39 +0000

    simplify example readability by removing outer function and calling query directly

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const client = createClient()
 // random strings to re-run script without encountering exclusive contraint error
 const randomString = () => Math.ceil(Math.random() * 10000).toString()
 
-const productsData = [
+const products = [
   {
     name: 'zapatilla Nike Air Power Mini 2',
     barcode: randomString(),
@@ -27,29 +27,23 @@ const productsData = [
   },
 ]
 
-export const insertProduct = async (products: typeof productsData) => {
-  const query = e.params({ items: e.json }, (params) => {
-    return e.for(e.json_array_unpack(params.items), (item) => {
-      return e.insert(e.Product, {
-        name: e.cast(e.str, item.name),
-        code: e.cast(e.str, item.code),
-        familyCode: e.cast(e.str, item.familyCode),
-        barcode: e.cast(e.str, item.barcode),
-        color: e.for(
-          e.set(e.json_get(item, 'color', 'name')),
-          (optionalItem) => {
-            return e.insert(e.Color, {
-              name: e.cast(e.str, optionalItem),
-              code: 'test',
-            })
-          }
-        ),
-      })
+const query = e.params({ items: e.json }, (params) => {
+  return e.for(e.json_array_unpack(params.items), (item) => {
+    return e.insert(e.Product, {
+      name: e.cast(e.str, item.name),
+      code: e.cast(e.str, item.code),
+      familyCode: e.cast(e.str, item.familyCode),
+      barcode: e.cast(e.str, item.barcode),
+      color: e.for(e.set(e.json_get(item, 'color', 'name')), (optionalItem) => {
+        return e.insert(e.Color, {
+          name: e.cast(e.str, optionalItem),
+          code: 'test',
+        })
+      }),
     })
   })
-  const result = await query.run(client, {
-    items: products,
-  })
-  console.log('result----> ', result)
-}
-insertProduct(productsData)
+})
+const result = await query.run(client, {
+  items: products,
+})
+console.log('result----> ', result)
