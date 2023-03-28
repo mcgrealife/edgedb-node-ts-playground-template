92647c5b231c5c9e24a407b850eb12c27ca1e2c7
Michael McGreal <mcgrealife@gmail.com>
2023-03-28 00:09:39 +0000

    ah but both if/else branch must be same type. So best solution is likely portable shapes to share logic between similar queries.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 // To view your database, `npm run ui`
 
 import { createClient } from 'edgedb'
-import e from '../dbschema/edgeql-js/index.mjs'
+import e, { $infer } from '../dbschema/edgeql-js/index.mjs'
 
 const client = createClient()
 
@@ -17,12 +17,16 @@ const query = e.params(
       'if',
       e.op($.split, '=', e.bool(true)),
       'else',
-      e.select(e.str('false'))
+      e.select(e.str('false')) // oh the if/else branches must be of same type though
+      // e.select(e.bool(false)) // returning bool here does not cause inferred type to be a union of `string | boolean`. // next best is probably portable shapes to resuse logic between queries
     )
 )
+
+type inferredType = $infer<typeof query> // string
 
 const expectTrue = await query.run(client, { split: false })
 const expectFalse = await query.run(client, { split: true })
 
 console.log('expectTrue:', expectTrue)
 console.log('expectFalse:', expectFalse)
+console.log()
